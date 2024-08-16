3fd5d265aa1b5ad0e5ce4fc8f19ccc8b1d4316e4
Christian Donoso <christ.donoso@gmail.com>
2024-08-16 12:31:46 -0400

    commit 1

--- crud_app/settings.py
+++ crud_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(f5e9mdo%$-vxtahdw#^3olkgczxv8o*m=tz8x38y-#gc=cez!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
